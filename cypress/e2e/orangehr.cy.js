557014562b846a33686befb2098813aeb6ef6065
Gabriel de Oliveira <102867333+Zokaaah@users.noreply.github.com>
2025-03-11 17:48:27 -0300

    preConfig

--- cypress/e2e/orangehr.cy.js
+++ cypress/e2e/orangehr.cy.js
@@ -1,5 +1,25 @@
 describe('Acessar tela de login OrangeHR', () => {
-  it('passes', () => {
-    cy.visit('zokah-osondemand.orangehrm.com')
+  it('login Invalido', () => {
+    const InvalidUser = Cypress.env('InvalidUser')
+    const InvalidPassword = Cypress.env('InvalidPassword')
+    const LinkAcesso = Cypress.env('LINK')
+    cy.visit(LinkAcesso)
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(InvalidUser)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(InvalidPassword)
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').should('contain','Invalid credentials')
+
+  })
+  it.only('login valido', () => {
+    const username = Cypress.env('USER')
+    const password = Cypress.env('password')
+    const LinkAcesso = Cypress.env('LINK')
+    cy.visit(LinkAcesso)
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+    cy.get('.oxd-button').click()
+
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('contain', 'Dashboard')
+
   })
 })
